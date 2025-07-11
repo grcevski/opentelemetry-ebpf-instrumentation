6087e9fd271253dab7a6e732a83069e65ab9a570
Nikola Grcevski <grcevski@gmail.com>
2025-07-11 13:06:48 -0400

    some other utils

--- bpf/generictracer/java_tls.c
+++ bpf/generictracer/java_tls.c
@@ -8,6 +8,7 @@
 #include <common/protocol_defs.h>
 
 #include <generictracer/k_tracer_defs.h>
+#include <generictracer/maps/pid_tid_to_conn.h>
 
 #include <logger/bpf_dbg.h>
 
@@ -40,6 +41,8 @@ int BPF_KPROBE(
         return 0;
     }
 
+    bpf_dbg_printk("=== do_vfs_ioctl id=%d ===", id);
+
     // it must be fd == 0 if we are considering this request
     if (fd) {
         return 0;
@@ -70,6 +73,15 @@ int BPF_KPROBE(
     bpf_probe_read(&p_conn.conn, sizeof(connection_info_t), arg + 1);
     u16 orig_dport = p_conn.conn.d_port;
     sort_connection_info(&p_conn.conn);
+    p_conn.pid = pid_from_pid_tgid(id);
+
+    // if (is_empty_connection_info(&p_conn.conn)) {
+    //     ssl_pid_connection_info_t *l = bpf_map_lookup_elem(&pid_tid_to_conn, &id);
+    //     bpf_dbg_printk("lookup for empty connection info %llx", l);
+    //     if (l) {
+    //         __builtin_memcpy(&p_conn, &l->p_conn, sizeof(pid_connection_info_t));
+    //     }
+    // }
 
     u32 len = 0;
     bpf_probe_read(&len, sizeof(u32), arg + 1 + sizeof(connection_info_t));
